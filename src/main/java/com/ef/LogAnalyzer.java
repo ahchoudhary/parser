4baef757ab755681345f1a0b6c375abae4a521f8
A Husain <husain@As-MacBook-Pro.local>
2018-09-20 11:47:40 -0700

    fixed threshold filtering

--- src/main/java/com/ef/LogAnalyzer.java
+++ src/main/java/com/ef/LogAnalyzer.java
@@ -10,7 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -25,26 +28,40 @@ public class LogAnalyzer {
     @Autowired
     LogAnalysisRepository logAnalysisRepository;
 
-//take the input arguments
-// query Database for that.
-//update the analysis table.
-// return related rows.
-//print the rows in console.
-
     public void analyze(LocalDateTime startDate, String duration, Long threshold){
 
-        LocalDateTime endDate = (Constants.HOURLY.equals(duration))? startDate.plusHours(1) : startDate.plusDays(1);
-        List<LogEntry> logEntries = logEntryRepository.findBlockedIp(java.sql.Timestamp.valueOf(startDate),
-                java.sql.Timestamp.valueOf(endDate));
+        List<LogEntry> logEntries = getLogEntries(startDate, duration, threshold);
 
         List<LogAnalysis> analyizedList = logEntries.stream().map(EntryToAnalysisMapper::mapLogEntry).collect(Collectors.toList());
         writeToConsole(analyizedList);
         logAnalysisRepository.saveAll(analyizedList);
 
     }
+    private List<LogEntry> getLogEntries(LocalDateTime startDate, String duration, Long threshold){
+        LocalDateTime endDate = (Constants.HOURLY.equals(duration))? startDate.plusHours(1) : startDate.plusDays(1);
+        List<LogEntry> dbEntries = logEntryRepository.findBlockedIp(java.sql.Timestamp.valueOf(startDate),
+                java.sql.Timestamp.valueOf(endDate));
+        Map<String, Long> ipCounts = new HashMap();
+        for(LogEntry x: dbEntries){
+            if(ipCounts.containsKey(x.getIp())){
+                ipCounts.put(x.getIp(), ipCounts.get(x.getIp())+1);
+            } else{
+                ipCounts.put(x.getIp(), 1l);
+            }
+        }
+
+        final Map<String, Long> thresholdCounts = ipCounts.entrySet().stream()
+                .filter((entry)-> entry.getValue() > threshold.longValue())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        return dbEntries.stream().filter(x -> thresholdCounts.containsKey(x.getIp())).collect(Collectors.toList());
+
+    }
+
     private void writeToConsole(List<LogAnalysis>  analyizedList){
-        System.out.println("\n\n blocked IPs are below:\n\n");
-        analyizedList.stream().forEach(x -> System.out.printf("ip: %s    comments: %s",x.getIp(), x.getComment()));
+        String heading = (analyizedList.isEmpty())? "Nothing to display" : "Blocked IPs are below";
+        System.out.println("\n\n "+ heading +":\n\n");
+        analyizedList.stream().forEach(x -> System.out.printf("ip: %s    comments: %s\n",x.getIp(), x.getComment()));
 
     }
 }
